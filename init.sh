6069483538ddf6a7e2c745a9ca283f97eea998b8
Rutador <rutador@gmail.com>
2020-08-22 17:59:30 +0300

    next wsgi

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo gunicorn --bind='0.0.0.0.8080' hello:app
+sudo .etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
